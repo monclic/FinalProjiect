a98657d26bcfcc65168304446760d66bd7233358
monclic <1727263980@qq.com>
2023-03-30 10:48:18 +0800

    滚动+nav例子

--- User_UI/src/components/test.vue
+++ User_UI/src/components/test.vue
@@ -1,140 +1,129 @@
 <template>
-  <div id="container">
-    <header id="header">
-      <div><a href="#">Link 1</a></div>
-      <div><a href="#">Link 1</a></div>
-    </header>
-
-    <main id="main">
-      <!-- nav -->
-      <div id="nav">
-        <nav class="nav-list">
-          <a class="nav-item">
-
-            <span>111111</span>
-
-          </a>
-        </nav>
-      </div>
-      <!-- content -->
-      <div id="content">
-        <!-- for  -->
-        <!-- div.item  -->
-        <!-- template  -->
-        <!-- /for -->
-        <Section/>
-        <Section/>
-        </div>
-
-     
+  <div class="container">
+    <nav>
+      <ul>
+        <!-- 如果 isActive(index) 返回 true，则该段落的 class 属性将被设置为 "active"，否则它将被设置为空字符串。 -->
+        <li v-for="(item, index) in items" :key="index" @click="scrollToSection(index)"  :class="{ active: isActive(index) }">
+         {{ item.title }}
+        </li>
+      </ul>
+    </nav>
+    <main class="main">
+      <section v-for="(item, index) in items" :key="index" :id="item.id">
+        <h2>{{ item.title }}</h2>
+        <p>{{ item.content }}</p>
+      </section>
     </main>
-
-    <footer id="footer"></footer>
   </div>
 </template>
 
-<script lang='ts' setup>
-import Section from './section.vue'
-</script>
+<script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 
-<style scoped>
-#container {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  /* 水平居中 */
-  justify-content: center;
-  /* 竖直居中 */
-  align-items: center;
-  padding-top: 4.6rem;
-  background-color: #e7e7e7;
+// 定义数据类型
+interface Item {
+  title: string
+  content: string
+  id: string
 }
 
-#header {
-  height: 3.6rem;
-  /* 固定 */
+// 定义一个包含标题、内容和ID的项目数组
+const items: Item[] = [
+  { title: 'Section 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', id: 'section-1' },
+  { title: 'Section 2', content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', id: 'section-2' },
+  { title: 'Section 3', content: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', id: 'section-3' },
+  { title: 'Section 4', content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', id: 'section-4' },
+]
+
+// 定义响应式数据activeIndex
+const activeIndex = ref(0)
+
+// 判断当前选项卡是否被选中
+const isActive = (index: number) => {
+  return activeIndex.value === index
+}
+
+// 设置当前选项卡为选中状态
+const setActive = (index: number) => {
+  activeIndex.value = index 
+}
+
+// 1.点击目录进行内容滚动
+const scrollToSection = (index: number) => { 
+  const pageScroll = document.querySelector(".main") as HTMLElement;   
+  if(pageScroll){
+    window.scrollTo({top:(<HTMLElement>pageScroll.children[index]).offsetTop,behavior:"smooth"});  
+};
+
+// 2.监听页面滚动事件
+const handleScroll = () => {
+  //  获取页面上所有的 section 元素
+  const sections = document.querySelectorAll('section')  
+
+// 获取页面滚动距离。window.pageYOffset、document.documentElement.scrollTop 和 document.body.scrollTop 分别对应了不同浏览器下获取页面滚动距离的方法，|| 0 避免了在不支持某种方法的浏览器下出现错误。
+const scrollTop = window.scrollY
+
+  // 倒序循环每个 section 元素，依次判断其是否在视口中。
+  for (let i = sections.length - 1; i >= 0; i--) {
+    const section = sections[i]
+    // 对于每个 section 元素，使用 getBoundingClientRect() 方法获取其在视口中的位置信息，包括距离视口顶部的偏移量等。
+    const rect = section.getBoundingClientRect()
+
+    // 判断页面滚动距离是否超过当前选项卡的位置
+    // 400 是一个偏移量的值，用于使选中的选项卡能够在滚动到目标位置之前就已经被激活。
+    // 由于是倒序遍历，且符合条件直接退出，不会出现被第一个nav一直占用的情况
+    if (rect.top <=400) {     
+      setActive(i)
+      break
+    }
+  }
+}
+
+// 监听组件挂载事件
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+  
+})
+
+// 监听组件卸载事件
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+</script>
+<style>
+nav {
   position: fixed;
   top: 0;
   left: 0;
-  right: 0;
-  padding: 10px 300px 10px;
-  background-color: #f5f5f5;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  /* 置于最前 */
-  z-index: 999;
-  /* 透明度 */
-  opacity: 0.5;
-  border-bottom: 1px solid #f5f5f5;
+  width: 200px;
+  height: 100vh;
+  background-color: #f1f1f1;
+  overflow: auto;
 }
 
-#main {
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 100%;
-  padding: 0 100px;
-}
-
-#nav {
-  order: 1;
-  flex-shrink: 0;
-  margin-right: 1.236rem;
-  height: auto;
-  width: 160px;
-  overflow: hidden;
-}
-
-.nav-list {
-  width: 160px;
-  padding: 0;
+nav ul {
+  list-style: none;
   margin: 0;
-  position: fixed;
+  padding: 0;
 }
 
-.nav-item {
-  /* 解决子元素超出父元素的范围的问题 */
-  /* box-sizing: border-box; */
-  display: flex;
-  align-items: center;
-  position: relative;
-  color: red;
-  background-color: #e4e1e122;
-  width: 100%;
-  /* em 单位是相对于当前元素的字体大小来计算的长度单位。如果一个元素没有定义字体大小，它将继承父元素的字体大小。 */
-  height: 3em;
-  margin-bottom: 0.618rem;
-  padding: 0 1.236rem;
-  border-radius: 3px;
-  overflow: hidden;
-  /* 用于去除文本的下划线或其他修饰效果 */
-  text-decoration: none;
-  /* 用于控制字母之间的距离 */
-  letter-spacing: .5px;
-  /* will-change用于告知浏览器某个元素将要发生改变，以便浏览器在元素状态改变之前进行优化，提升性能。 */
-  will-change: background-color;
-  transition: background-color .06s;
+nav li {
+  padding: 10px;
+  transition: background-color 0.3s ease;
+  cursor: pointer;
 }
 
-.nav-item:hover {
-  /* 悬停变色 */
-  background-color: #fff;
+nav li.active {
+  background-color: #ddd;
 }
 
-.nav-info {
-  display: flex;
-  overflow: hidden;
-  height: 2.3rem;
-  width: 100%;
-  background-color: #f3f3f2;
-  transition: background-color .1s;
+main {
+  margin-left: 200px;
+  padding: 20px;
 }
 
-#content {
-  order: 2;  
-  position: relative;
-  overflow: hidden;
+section {
+  margin-bottom: 200px;
+  height: 1000px;
 }
 </style>
