626b37d52bf3025aaa422d9f881f76e1249e8c7e
monclic <1727263980@qq.com>
2023-05-09 15:57:55 +0800

    ### 上拉加载 父元素无固定高度

--- User_UI/src/components/testVV.vue
+++ User_UI/src/components/testVV.vue
@@ -1,22 +1,57 @@
 <template>
-  <p>2222222222</p>
-  <span>1111110{{ message }}</span>
+  <div>
+    <div v-for="(item, index) in items" :key="index" class="item">
+      <div style="height: 100px;"></div>{{ item }}</div>
+    <div ref="sentinel" class="sentinel"></div>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
+<script lang="ts" setup>
+import { ref, onMounted, onUnmounted, nextTick } from 'vue';
 
-export default defineComponent({
-  name: 'MyComponent',
+const items = ref([1, 2, 3]);
+const sentinel = ref<HTMLDivElement | null>(null);
+const loading = ref(false);
 
+const observer = new IntersectionObserver(([entry]) => {
+  if (entry.isIntersecting) {
+    loadMore();
+  }
 });
+
+onMounted(() => {
+  console.log(sentinel.value);
+  
+  if (sentinel.value) {
+    observer.observe(sentinel.value);
+  }
+});
+
+onUnmounted(() => {
+  observer.disconnect();
+});
+
+const loadMore = async () => {
+  if (loading.value) return;
+  loading.value = true;
+  await nextTick();
+  items.value.push(...[1, 2, 3]);
+  loading.value = false;
+};
+
 </script>
-<script lang='ts' setup>
- const message = ref(1);
-   function c(){
-      message.value+=1;
-    }
-    // defineExpose({ c })
-    // 把 message 暴露出去
-    defineExpose({ c ,message})
-</script>
+
+<style scoped>
+.scrollable {
+  /* height: 100vh; */
+  overflow-y: auto;
+}
+.item {
+  /* height: 150px; */
+  background-color: #ddd;
+  margin-bottom: 10px;
+}
+.sentinel {
+  height: 20px;
+}
+</style>
